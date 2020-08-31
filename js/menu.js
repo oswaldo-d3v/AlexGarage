f22bb3d11fd871ebb81edd280fa0e24bf43c32aa
Miguel Oswaldo <moswaldoec01@gmail.com>
2020-08-31 14:46:31 -0600

    Version mobil lista

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function showHidenMenu() {
+    var x = document.getElementById("menu-navegacion");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  } 
