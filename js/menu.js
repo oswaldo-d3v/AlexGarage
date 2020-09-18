0e3986e49d12da2b9f556721e7e50781f05dfee2
Miguel Oswaldo Escobar Cuellar <moswaldoec01@gmail.com>
2020-09-18 12:25:05 -0600

    No alamcena cache, usa cookies para recordar idioma, falta redaccion

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,13 @@
 function showHidenMenu() {
-    var x = document.getElementById("menu-navegacion");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-  } 
+  var x = document.getElementById("menu-navegacion");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
+
+function cambiarIdioma(idioma, pagina) {
+  document.cookie = "lenguaje=" + idioma + ";expires=Thu, 12 Aug 2049 20:47:11 UTC;";
+  location.href = pagina + ".html";
+}
